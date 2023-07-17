0ce2c866b1bec3f1a2b0a15d358e24e2387b8348
Naturalis007 <tamara.salasvergara@gmail.com>
2023-07-17 16:01:59 +0000

    arreglos en admin.py

--- src/admin.py
+++ src/admin.py
@@ -13,6 +13,8 @@ def setup_admin(app):
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(People, db.session))
     admin.add_view(ModelView(Fav_People, db.session))
+    admin.add_view(ModelView(Fav_Planets, db.session))
+
 
 
     # You can duplicate that line to add mew models
